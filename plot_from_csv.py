f64075b7efa8827307b7cd09f4432de19e588387
Franziska Mueller <franziska.mueller1@gmail.com>
2024-04-30 19:53:13 +0200

    feat(try,plot): plots try data for all 3 representative year scenarios by...
      * ...reading 3 TRY datafiles for
        * average year,
        * year with extreme summer and
        * year with extreme winter,
      * writing it into a single CSV
      * plotting it in one graph via plot_from_csv

--- plot_from_csv.py
+++ plot_from_csv.py
@@ -2,33 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataframe = pd.read_csv("out/hostrada.txt", sep=";")
+#dataframe = pd.read_csv("out/hostrada.txt", sep=";")
+#dataframe = pd.read_csv("out/try.csv", sep=";", header=None, names=["time", "temperature"])
+dataframe = pd.read_csv("out/try.csv", sep=";", header=None, names=["time", "temperature_year",
+                                                                    "temperature_summer", "temperature_winter"])
 print(dataframe)
 
 
+# dataframe.plot(kind='line',
+#                x='time',
+#                y='temperature',
+#                lw=1
+#                )
+# plt.title('Hostrada 2023, Loebauer 46')
+
 dataframe.plot(kind='line',
                x='time',
-               y='temperature',
+               y=["temperature_year",
+                  "temperature_summer",
+                  "temperature_winter"],
+               color=['pink','red', 'blue'],
                lw=1
                )
-plt.title('Hostrada 2023, Loebauer 46')
+plt.title('TRY 2045, Loebauer 46')
 
 plt.axhline(20, color='blue', lw=0.5)
 plt.axhline(30, color='red', lw=0.5)
 plt.axvline(2190, color='lightgray', lw=1)
 plt.axvline(4380, color='lightgray', lw=0.5)
 plt.axvline(6570, color='lightgray', lw=0.5)
+plt.ylabel('Temperature [°C]')
+plt.xlabel('Date and Time [mm-dd-hh]')
+plt.minorticks_on()
+# plt.grid(True)
 
 plt.show()
 
 # with open("out/test.txt", 'a') as outfile:
 #    outfile.write("test")
-
-
-x = np.arange(0, 31, 1/24)
-y = np.arange(0, 31, 1/24)
-
-plot = plt.plot(x, y)
+#
+#
+# x = np.arange(0, 31, 1/24)
+# y = np.arange(0, 31, 1/24)
+#
+# plot = plt.plot(x, y)
 # fig = plt.figure()
 
 # plt.show()
--- read_try.py
+++ read_try.py
@@ -1,25 +1,57 @@
 import sys
 
-infile_original_try = sys.argv[1]
+# infile_original_try = sys.argv[1]
+path_to_try_files = '../../data/try/TRY_513564124162_Loebauer/'
+try_files = ['TRY2045_513564124162_Jahr.dat', 'TRY2045_513564124162_Somm.dat', 'TRY2045_513564124162_Wint.dat']
+
+
+# parse file for dates and temperatures only
+def times_and_temperatures_from_file(infile_original_try):
+    return select_columns_from_file(infile_original_try, 2, 6)
+
+
+def temperatures_from_file(file):
+    return select_columns_from_file(file,5, 6)
+
+
+def select_columns_from_file(infile_original_try, first_column, last_column_exlusive):
+    data = []
+    with open(infile_original_try, 'r') as try_file:
+        for line in try_file:
+
+            # if line starts wiht letter or whitespace --> skip, because header
+            if line and is_part_of_header(line):
+                pass
+            else:
+                row = line.split()
+                # only keep datetime and temperature
+                data.append(row[first_column:last_column_exlusive])
+    return data
 
 
 def is_part_of_header(line):
     return line[0].isalpha() or line[0].isspace() or line[0] == "*"
 
 
-data = []
+data_year = times_and_temperatures_from_file(path_to_try_files + try_files[0])
+data_summ = temperatures_from_file(path_to_try_files + try_files[1])
+data_wint = temperatures_from_file(path_to_try_files + try_files[2])
 
-# parse file for temperatures only
-with open(infile_original_try, 'r') as try_file:
-    for line in try_file:
+lines = []
+for i, row in enumerate(data_year):
+    datetime = '-'.join(row[0:3])
+    temperatures = ';'.join([row[3], data_summ[i][0], data_wint[i][0]])
+    line = datetime + ';' + temperatures + '\n'
+    lines.append(line)
 
-        # if line starts wiht letter or whitespace --> skip, because header
-        if line and is_part_of_header(line):
-            pass
-        else:
-            row = line.split()
-            data.append(row)
+with open("out/try.csv", 'a') as outfile:
+    outfile.writelines(lines)
 
-print(data[0:5])
-            
-
+#      RW      HW MM DD HH     t    p  WR   WG N    x  RF    B    D   A    E IL
+# ***
+# 4162500 2733500  1  1  1   0.7 1006 245  0.5 7  3.5  85    0    0 291 -281  0
+# 4162500 2733500  1  1  2   0.4 1006 242  0.6 7  3.4  85    0    0 287 -280  0
+# 4162500 2733500  1  1  3   0.2 1005 239  0.6 7  3.3  85    0    0 282 -279  0
+# 4162500 2733500  1  1  4   0.3 1004 240  0.6 7  3.3  84    0    0 280 -277  0
+#
+# [['1', '1', '1', '0.7'], ['1', '1', '2', '0.4'], ['1', '1', '3', '0.2'],
