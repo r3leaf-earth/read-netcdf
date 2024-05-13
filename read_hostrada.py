4756bcf21c47001e2f2a1ae91a4553a4583f269e
Franziska Mueller <franziska.mueller1@gmail.com>
2024-05-13 16:40:39 +0200

    refactor(hostrada): improves usability and readability for read_hostrada.py

--- read_hostrada.py
+++ read_hostrada.py
@@ -27,6 +27,7 @@
 #   * a file from HOSTRADA: https://opendata.dwd.de/climate_environment/CDC/grids_germany/hourly/hostrada/
 #     * has been downloaded
 #     * which has not been renamed
+#   * geocoordinates have been changed to your desired location in Germany (hostrada is for Gemany only)
 #
 #
 # Usage:
@@ -35,11 +36,27 @@
 #     `python read_hostrada.py "path/to/uhi_1hr_HOSTRADA-v1-0_BE_gn_2023050100-2023053123.nc"`
 #   * see csv file and if you want, plot it with `python plot_hostrada_from_csv.py`
 #
+# NOTE:
+#  * if you do not rename your outfile, this script will append to it (if it exists)
+#  * this is desired behavior. It is used for accumulating more than one month (the hostrada files are per month)
+#    into one output file. This way you could, for exaple, plot a whole year from one csv file later.
+#
 #############################################################
 import os.path
 import sys
+
 import netCDF4
 import numpy as np
+
+# TO BE CHANGED BY USER, IF DESIRED
+path_to_outfile = "out/hostrada.csv"
+
+location_lat = 51.35618  # <--- change to your desired location latitude, here
+location_lon = 12.41687  # <--- change to your desired location longitude, here
+
+# Berlin Alexanderplatz 52.52165/13.41144
+# Cologne 50.9380/6.9572
+# Leipzig Loebauer 51.35618/12.41687
 
 
 # GET THE FILE
@@ -47,10 +64,15 @@ path_to_file = sys.argv[1]
 print("Converting file to dataset...")
 dataset = netCDF4.Dataset(path_to_file, 'r')
 
+
+def climate_variable_name_from_path_to_file(path_to_file):
+    filename = path_to_file.split("/")[-1]
+    words_in_filename = filename.split("_")
+    return words_in_filename[0]
+
+
 # THE MAIN VARIABLE
-filename = path_to_file.split("/")[-1]
-words_in_filename = filename.split("_")
-climate_variable_name = words_in_filename[0]
+climate_variable_name = climate_variable_name_from_path_to_file(path_to_file)
 print("Main variable name is: ", climate_variable_name)
 climate_variable = dataset.variables[climate_variable_name]
 
@@ -77,13 +99,6 @@ def getclosest_gridpoints_indices(lats, lons, desired_lat, desired_lon):
     return np.unravel_index(minindex_flattened, lats.shape)
 
 
-location_lat = 51.35618      # <--- change to your desired location latitude, here
-location_lon = 12.41687      # <--- change to your desired location longitude, here
-
-# Berlin Alexnderplatz 52.52165/13.41144
-# Cologne 50.9380/6.9572
-# Leipzig Loebauer  51.35618/12.41687
-
 print("Searching grid point for location: lat: ", location_lat, ", lon: ", location_lon)
 grid_lat_index, grid_lon_index = getclosest_gridpoints_indices(latvals, lonvals, location_lat, location_lon)
 print("Closest grid point found is:", lat[grid_lat_index, grid_lon_index], lon[grid_lat_index, grid_lon_index])
@@ -100,10 +115,8 @@ ds_times = dataset.variables['time']
 times_as_dates = netCDF4.num2date(ds_times[:], ds_times.units, ds_times.calendar)
 
 
-# (PRINT) DATE WITH ITS PREDICTED TEMPERATURE (or other climate variable) AND WRITE TO FILE
-
+# WRITE TO FILE (or PRINT) DATE WITH ITS PREDICTED TEMPERATURE (or other climate variable)
 # if newfile write header, therefore:
-path_to_outfile = "out/hostrada.csv"
 exists = os.path.exists(path_to_outfile)
 is_newfile = not exists
 
